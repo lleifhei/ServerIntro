587667cf63e7e9ba62f7cfc26ea8d3d16bc0f466
lleifhei <loganleifheit@gmail.com>
2025-04-17 11:35:52 -0400

    Server intro WIP

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+const pets = [
+    { name: 'Hans', species: 'Dog'},
+    { name: 'Smokey', species: 'Cat'},
+  ];
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+    });
+app.get('/pets', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pets.html'));
+    }
+);
+app.get('/pets/:name', (req, res) => {
+    const petName = req.params.name;
+    const pet = pets.find(p => p.name.toLowerCase() === petName.toLowerCase());
+    if (pet) {
+      res.status(200).json(pet);
+    } else {
+      res.status(404).send('Pet not found');
+    }
+  }
+);
+app.get('/api/pets', (req, res) => {
+    res.json(pets);
+  }
+);
+app.get('/api/pets/:name', (req, res) => {
+    const petName = req.params.name;
+    const pet = pets.find(p => p.name.toLowerCase() === petName.toLowerCase());
+    if (pet) {
+      res.status(200).json(pet);
+    } else {
+      res.status(404).send('Pet not found');
+    }
+  }
+);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
